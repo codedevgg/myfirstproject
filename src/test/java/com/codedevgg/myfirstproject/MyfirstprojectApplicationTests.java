6817785489e2722453341cb309e60976c66e4aff
codedevgg <ginogrecor@gmail.com>
2022-01-26 21:43:48 -0400

    category memory

---
+++ src/test/java/com/codedevgg/myfirstproject/MyfirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedevgg.myfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
